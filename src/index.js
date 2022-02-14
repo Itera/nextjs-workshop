ca455719039b939aae23ba109ece7fcdc89da7a6
Tin Anh Nguyen <anhtin95@gmail.com>
2022-02-14 12:29:22 +0100

    Add missing CSS reset

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Reveal from "reveal.js";
+import "reveal.js/dist/reset.css";
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/black.css";
 import Highlight from "reveal.js/plugin/highlight/highlight";
